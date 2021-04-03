9afbd497c9a513499d3c1745a3ca1c03c0e174fc
rustamov-bite <assasins_murza006@mail.ru>
2021-04-03 11:57:09 +0600

    added registerUser method to UserService

--- src/main/java/com/contrwork/forum/services/UserService.java
+++ src/main/java/com/contrwork/forum/services/UserService.java
@@ -1,5 +1,8 @@
 package com.contrwork.forum.services;
 
+import com.contrwork.forum.configs.SecurityConfig;
+import com.contrwork.forum.forms.RegisterForm;
+import com.contrwork.forum.models.User;
 import com.contrwork.forum.repos.UserRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserService {
     private final UserRepo userRepo;
+
+    public void registerUser(RegisterForm registerForm) {
+        User user = User.builder()
+                .email(registerForm.getEmail())
+                .name(registerForm.getName())
+                .password(SecurityConfig.encoder().encode(registerForm.getPassword()))
+                .build();
+        userRepo.save(user);
+    }
 }
