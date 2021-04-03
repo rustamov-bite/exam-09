1f2c0201b5ed515efc00d83ed4e0cfc38a31b864
rustamov-bite <assasins_murza006@mail.ru>
2021-04-03 18:01:55 +0600

    updated repositories

--- src/main/java/com/contrwork/forum/repos/AnswerRepo.java
+++ src/main/java/com/contrwork/forum/repos/AnswerRepo.java
@@ -1,9 +1,16 @@
 package com.contrwork.forum.repos;
 
 import com.contrwork.forum.models.Answer;
+import com.contrwork.forum.models.Theme;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepo extends JpaRepository<Answer, Long> {
+    Page<Answer> findAnswersByTheme(Theme theme, Pageable pageable);
+    List<Answer> findAnswersByTheme(Theme theme);
 }
--- src/main/java/com/contrwork/forum/repos/ThemeRepo.java
+++ src/main/java/com/contrwork/forum/repos/ThemeRepo.java
@@ -1,6 +1,7 @@
 package com.contrwork.forum.repos;
 
 import com.contrwork.forum.models.Theme;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
